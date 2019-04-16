a90e153a109975a7eba2f1a5d78431f35bd2ffc6
tommy <to.zawistowski@gmail.com>
2019-04-16 11:17:30 +0200

    Fixed templates + home page added

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
 	path('budget/', include('budget.urls')),
     path('admin/', admin.site.urls),
     path('', include('budget.urls')),
