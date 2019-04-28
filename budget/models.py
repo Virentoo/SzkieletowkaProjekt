5988a37d12a9cd92c04a6148dec8fae7134e4696
Virento <namishis2@gmail.com>
2019-04-28 19:17:57 +0200

    new branch
    #Added /base url
    #Added models short name
    #Created style for base in static/css

--- budget/models.py
+++ budget/models.py
@@ -6,11 +6,17 @@ class User(models.Model):
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.login
+
 
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, default="")
     monthly = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s - %s" % (self.user, self.name)
 
 
 class Income(models.Model):
@@ -20,6 +26,9 @@ class Income(models.Model):
     income_date = models.DateTimeField('Income added')
     amount = models.FloatField(default=0)
 
+    def __str__(self):
+        return self.income_name
+
 
 class Expense(models.Model):
     expense_name = models.CharField(max_length=100)
@@ -27,3 +36,6 @@ class Expense(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     expense_date = models.DateTimeField('Expense added')
     amount = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.expense_name
