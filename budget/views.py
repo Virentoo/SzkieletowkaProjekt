d7e6ed155ce7bc6bb0fc906b59ba716a36f62bbf
Virento <namishis2@gmail.com>
2019-04-28 20:28:19 +0200

    new branch
    #

--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from budget.models import Income
 
 
 def base(request):
-    return render(request, 'budget/base.html')
+    income_list = Income.objects.order_by('-income_date')
+    context = {'income_list': income_list}
+    return render(request, 'budget/base.html', context)
 
 
 def home(request):
