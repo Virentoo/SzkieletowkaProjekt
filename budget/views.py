275ec3a400c6dff4e8dd78408a3637402d826848
Virento <namishis2@gmail.com>
2019-04-29 01:00:27 +0200

    new branch
    #

--- budget/views.py
+++ budget/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from budget.models import Income
+from budget.models import Income, Expense
 
 
 def base(request):
-    income_list = Income.objects.order_by('-income_date')
-    context = {'income_list': income_list}
-    return render(request, 'budget/base.html', context)
+    if request.method == "POST":
+        selected_id = request.POST['dropdown_panel']
+    else:
+        selected_id = 1
+
+    if selected_id == "2":
+        expense_list = Expense.objects.order_by('-expense_date')
+        context = {'expense_list': expense_list, 'selected_panel': 'Expense', }
+        return render(request, 'budget/base_expense.html', context)
+    else:
+        income_list = Income.objects.order_by('-income_date')
+        context = {'income_list': income_list, 'selected_panel': 'Income', }
+        return render(request, 'budget/base_income.html', context)
 
 
 def home(request):
