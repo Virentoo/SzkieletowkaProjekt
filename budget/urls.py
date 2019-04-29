e441d42d0e8660524bbaab5101b9b5e1ea56df74
Virento <namishis2@gmail.com>
2019-04-29 22:02:56 +0200

    new branch
    #

--- budget/urls.py
+++ budget/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('base', views.base, name='base'),
+    path('base/newincome', views.home, name='base/newincome'),
+    path('base/newexpense', views.home, name='base/newexpense'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,20 +1,26 @@
+import datetime
+
 from django.shortcuts import render
 from budget.models import Income, Expense
 
 
 def base(request):
+    date = datetime.datetime.now()
     if request.method == "POST":
         selected_id = request.POST['dropdown_panel']
     else:
         selected_id = 1
 
+    context = {'date': date}
     if selected_id == "2":
         expense_list = Expense.objects.order_by('-expense_date')
-        context = {'expense_list': expense_list, 'selected_panel': 'Expense', }
+        context['expense_list'] = expense_list
+        context['selected_panel'] = 'Expense'
         return render(request, 'budget/base_expense.html', context)
     else:
         income_list = Income.objects.order_by('-income_date')
-        context = {'income_list': income_list, 'selected_panel': 'Income', }
+        context['income_list'] = income_list
+        context['selected_panel'] = 'Income'
         return render(request, 'budget/base_income.html', context)
 
 
