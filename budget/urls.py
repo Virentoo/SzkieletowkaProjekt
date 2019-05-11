1388e15a0363d7dfaf83f06ae747e3bd226533cb
Virento <namishis2@gmail.com>
2019-05-11 12:00:32 +0200

    Renamed base to budget Added button 'Budget' to home site Added back icon button to profile

--- budget/urls.py
+++ budget/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('base', views.base, name='base'),
-    path('base/recent', views.base_recent, name='base/recent'),
-    path('base/new', views.home, name='base/new'),
+    path('budget', views.budget, name='budget'),
+    path('budget/recent', views.budget_recent, name='budget/recent'),
+    path('budget/new', views.home, name='budget/new'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -6,7 +6,20 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required()
-def base_recent(request):
+def budget(request):
+    item_list = {}
+    user = request.user
+    category_list = Category.objects.filter(user=user).order_by('-name')
+    context = {'category_list': category_list}
+    for item in category_list:
+        item_list[item] = (
+            Transaction.objects.filter(category=item))
+    context['item_list'] = item_list
+    return render(request, 'budget/budget.html', context)
+
+
+@login_required()
+def budget_recent(request):
     date = datetime.datetime.now()
     selected_id = request.POST.get('dropdown_panel', 1)
     user = request.user
@@ -20,21 +33,8 @@ def base_recent(request):
         income_list = Transaction.objects.filter(category__user=user, type="income").order_by('-date')
         context['transaction_list'] = income_list
         context['selected_panel'] = 'Income'
-    return render(request, 'budget/base.html', context)
+    return render(request, 'budget/budget_recent.html', context)
 
 
 def home(request):
     return render(request, 'budget/home.html')
-
-
-@login_required()
-def base(request):
-    item_list = {}
-    user = request.user
-    category_list = Category.objects.filter(user=user).order_by('-name')
-    context = {'category_list': category_list}
-    for item in category_list:
-        item_list[item] = (
-            Transaction.objects.filter(category=item))
-    context['item_list'] = item_list
-    return render(request, 'budget/base_recent.html', context)
