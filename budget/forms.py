e1f90b4f235702a79bde9b445e2f169aaf966057
Virento <namishis2@gmail.com>
2019-05-14 20:02:45 +0200

    Improved filtering system #Aplalying filters refresh transaction list without refreshing site #Added 'sort by' filter

--- budget/forms.py
+++ budget/forms.py
@@ -8,5 +8,9 @@ class FilterForm(forms.Form):
     dateTo = forms.DateField(required=False)
     timeFrom = forms.TimeField(required=False)
     timeTo = forms.TimeField(required=False)
+    transaction_type = forms.CharField(required=False)
+    recCategory = forms.CharField(required=False)
+    remCategory = forms.CharField(required=False)
+    monthly2 = forms.CharField(required=False)
 
 
--- budget/urls.py
+++ budget/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('budget', views.budget, name='budget'),
     path('budget/recent', views.budget_recent, name='budget/recent'),
     path('budget/new', views.home, name='budget/new'),
+    path('budget/get_budget/', views.get_budget, name='get_budget'),
 ]
