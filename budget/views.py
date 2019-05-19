f9540f4092050d474ddc5fa986e79f20123157d1
Igor Becz <igorbecz@pop-os.localdomain>
2019-05-19 13:39:16 +0200

    Added chart and add_new_category buttons

--- budget/views.py
+++ budget/views.py
@@ -7,6 +7,8 @@ from .forms import FilterForm
 from .utils import convert_datetime
 from django.http import Http404
 from django.http import HttpResponse
+from django.utils import timezone
+import datetime
 
 
 @login_required()
@@ -60,6 +62,19 @@ def new_expense(request):
 @login_required()
 def new_income(request):
     return render(request, 'budget/new_income.html')
+
+
+class NewCategoryView(CreateView):
+    model = Category
+    template_name = 'budget/new_category.html'
+    fields = ['name', 'monthly']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('budget')
 
 
 @login_required()
@@ -172,4 +187,53 @@ def delete(request):
         return HttpResponse("Wrong id")
     Transaction.objects.filter(category__user=request.user, id__in=id).delete()
     return HttpResponse("Success")
+
+
+@login_required()
+def chart(request):
+    user = request.user
+    user_id = user.id
+    category_list = Category.objects.filter(user=user).order_by('-name')
+    categories = list()
+    item_list = {}
+
+    for category in category_list:
+        categories.append(category.name)
+
+    categories_id = list()
+    for category in category_list:
+        categories_id.append(category.id)
+
+    user_transactions = []
+    sums_list_current = []
+    sums_list_previous = []
+
+    for n in categories_id:
+        user_transactions.append(Category.objects.get(id=n).transaction_set.all())
+
+    for n in user_transactions:
+        sum_current = 0
+        sum_previous = 0
+        for i in n:
+            if i.type in ["expense", "Expense"]:
+                if i.date >= timezone.now() - datetime.timedelta(days=30):
+                    sum_current += i.amount
+                if i.date <= timezone.now() - datetime.timedelta(days=30) and i.date >= timezone.now() - datetime.timedelta(days=60):
+                    sum_previous += i.amount
+        sums_list_current.append(sum_current)
+        sums_list_previous.append(sum_previous)
+
+    for item in category_list:
+        item_list[item] = (
+            Transaction.objects.filter(category=item))
+
+    return render(request, 'budget/chart.html',{
+        'categories': categories,
+        'categories_id': categories_id,
+        'item_list' : item_list,
+        'sums_list_current' : sums_list_current,
+        'sums_list_previous' : sums_list_previous,
+    })
+
+
 
